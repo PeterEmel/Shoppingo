181fe90d2610422512a3f9e4dad550b371d5d8e5
Peter Emel <peteremel@Peters-iMac.local>
2019-11-30 01:08:12 -0800

    First commit

---
+++ Pods/Target Support Files/Pods-Shoppingo/Pods-Shoppingo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Shoppingo : NSObject
+@end
+@implementation PodsDummy_Pods_Shoppingo
+@end
---
+++ Shoppingo/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shoppingo
+//
+//  Created by Peter Emel on 11/30/19.
+//  Copyright © 2019 Peter Emel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

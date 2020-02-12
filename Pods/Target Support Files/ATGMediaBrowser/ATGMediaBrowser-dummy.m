d626957cfb3b6bdade1effe19ea1e898929cfa88
Peter Emel <peteremel@Peters-iMac.local>
2020-02-12 06:57:27 -0800

    Tap bar & CartVC(Realm)

--- Pods/Target Support Files/ATGMediaBrowser/ATGMediaBrowser-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ATGMediaBrowser : NSObject
-@end
-@implementation PodsDummy_ATGMediaBrowser
-@end
---
+++ Shoppingo/Controller/AppContainerVC.swift
@@ -0,0 +1,22 @@
+//
+//  AppContainerVC.swift
+//  Shoppingo
+//
+//  Created by Peter Emel on 2/11/20.
+//  Copyright © 2020 Peter Emel. All rights reserved.
+//
+
+import UIKit
+
+class AppContainerVC: UIViewController {
+
+    override func viewDidAppear(_ animated:Bool) {
+        super.viewDidAppear(animated)
+
+        AppManager.shared.appContainer = self
+        AppManager.shared.showApp()
+    }
+    
+
+
+}
